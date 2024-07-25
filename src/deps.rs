2df353c1e2089d4d5819f117692b0bcbc57d748d
ZhiyuanSue <108735103+ZhiyuanSue@users.noreply.github.com>
2024-07-25 12:44:30 +0800

    try to clean the ffi kernel_stack_alloc ksIdleThreadTCB and Arch_finaliseCap (#9)

--- src/deps.rs
+++ src/deps.rs
@@ -3,8 +3,6 @@ use crate::structures::finaliseCap_ret;
 use sel4_common::structures::exception_t;
 
 extern "C" {
-    pub fn Arch_finaliseCap(cap: &cap_t, final_: bool) -> finaliseCap_ret;
-
     pub fn finaliseCap(cap: &cap_t, _final: bool, _exposed: bool) -> finaliseCap_ret;
 
     pub fn post_cap_deletion(cap: &cap_t);
